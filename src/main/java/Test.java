a4e95490ac291d23e6b08aee5ac07a3245b76dca
tanghaijia <893968908@qq.com>
2021-02-15 18:57:48 +0800

    初始静态/servlet服务器

--- src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.io.IOException;
-
-public class Test {
-    public static void main(String[] args) throws IOException {
-        File classPath = new File(Constants.WEB_ROOT);
-        System.out.println(classPath.getCanonicalPath());
-    }
-}
